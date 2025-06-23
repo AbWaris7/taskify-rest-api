dd59d1a58f2e4d429ec2e4487c16bab7b5ee5ae1
abwaris7 <hellokhan.ch@gmail.com>
2025-06-23 09:04:49 +0200

    Add Todo entity with JPA annotations and basic fields

--- src/main/java/com/khan/code/taskify_rest_api/entity/Todo.java
+++ src/main/java/com/khan/code/taskify_rest_api/entity/Todo.java
@@ -1,9 +1,29 @@
 package com.khan.code.taskify_rest_api.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
-@Table(name = "todo")
+import lombok.*;
+
 @Entity
+@Table(name = "todos")
+@Data // Generates getters, setters, toString, equals, hashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private int priority;
+
+    @Column(nullable = false)
+    private boolean complete;
 }
