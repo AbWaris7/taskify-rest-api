99f11eeddae53a534348a99e107bbd53cbc977f1
abwaris7 <hellokhan.ch@gmail.com>
2025-06-30 07:54:23 +0200

    Add login method to AuthenticationService interface

--- src/main/java/com/khan/code/taskify_rest_api/service/AuthenticationService.java
+++ src/main/java/com/khan/code/taskify_rest_api/service/AuthenticationService.java
@@ -1,8 +1,12 @@
 package com.khan.code.taskify_rest_api.service;
 
+import com.khan.code.taskify_rest_api.request.AuthenticationRequest;
 import com.khan.code.taskify_rest_api.request.RegisterRequest;
+import com.khan.code.taskify_rest_api.response.AuthenticationResponse;
 
 public interface AuthenticationService {
 
     void register(RegisterRequest input) throws Exception;
+
+    AuthenticationResponse login(AuthenticationRequest input);
 }
