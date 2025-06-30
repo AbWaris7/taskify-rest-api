b657a2eb20cecdbedcb8dd7eca5c7aa871902908
abwaris7 <hellokhan.ch@gmail.com>
2025-06-30 07:53:54 +0200

    Create AuthenticationResponse class with token field and accessors

---
+++ src/main/java/com/khan/code/taskify_rest_api/response/AuthenticationResponse.java
@@ -0,0 +1,19 @@
+package com.khan.code.taskify_rest_api.response;
+
+
+public class AuthenticationResponse {
+
+    private String token;
+
+    public AuthenticationResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
