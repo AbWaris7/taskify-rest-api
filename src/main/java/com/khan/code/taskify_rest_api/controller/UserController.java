e8884683938d33339b17bcf33cf0065c0b4bb553
abwaris7 <hellokhan.ch@gmail.com>
2025-07-01 06:30:40 +0200

    added User Controller

---
+++ src/main/java/com/khan/code/taskify_rest_api/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.khan.code.taskify_rest_api.controller;
+
+import com.khan.code.taskify_rest_api.entity.User;
+import com.khan.code.taskify_rest_api.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "User REST API Endpoints", description = "Operations related to ino about current user")
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/info")
+    public User getUserInfo() {
+
+        return userService.getUserInfo();
+    }
+}
