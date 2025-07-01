00324421666cfbeba1dd88135864a47b23308cef
abwaris7 <hellokhan.ch@gmail.com>
2025-07-02 00:02:41 +0200

    added delete mapping

--- src/main/java/com/khan/code/taskify_rest_api/controller/UserController.java
+++ src/main/java/com/khan/code/taskify_rest_api/controller/UserController.java
@@ -4,6 +4,7 @@ import com.khan.code.taskify_rest_api.response.UserResponse;
 import com.khan.code.taskify_rest_api.service.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +24,10 @@ public class UserController {
     public UserResponse getUserInfo() {
 
         return userService.getUserInfo();
+    }
+
+    @DeleteMapping
+    public void deleteUser() {
+        userService.deleteUser();
     }
 }
