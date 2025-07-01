f2d5974be15d8f1d02b7101ec4da38d8ff060a3e
abwaris7 <hellokhan.ch@gmail.com>
2025-07-02 00:00:23 +0200

    custom query to count admin

--- src/main/java/com/khan/code/taskify_rest_api/repository/UserRepository.java
+++ src/main/java/com/khan/code/taskify_rest_api/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.khan.code.taskify_rest_api.repository;
 
 import com.khan.code.taskify_rest_api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
@@ -10,5 +11,6 @@ public interface UserRepository extends CrudRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 
+    @Query("SELECT COUNT(u) from USER u join u.authorities a where a.authority = 'ROLE_ADMIN'")
     long countAdminUsers();
 }
